9b7164b594fd49da72c669e9c22533f81707ae36
siva <naveennaveen86602@gmail.com>
2025-08-13 11:44:27 +0530

    inital commit

---
+++ d1.js
@@ -0,0 +1,35 @@
+/*let a=10;
+let b=10;
+let result=a+b;
+console.log(result);
+let n="bharathi";
+console.log(n);
+{
+    var n1=15;
+    console.log(n1)
+
+}
+console.log(n1);
+var na="naveen";
+var na = "dasan";
+console.log(na);*/
+
+let a1=10;
+let b1="10";
+console.log(a1==b1);
+console.log(a1===b1);
+console.log(typeof(b1));
+console.log(a1!=b1);
+console.log(a1!==b1);
+console.log(++a1);
+console.log(a1);
+let age=20;
+let st=(age>=20)?"adult":"minor";
+console.log(st);
+let input=require('prompt-sync')();
+let age1=input("Enter Your Age:");
+
+
+
+
+
